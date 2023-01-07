dece7f30af92363324bc006bd7496da61bfd4158
pandekar <pandeadhistanaya@gmail.com>
2023-01-07 16:21:22 +0800

    slider functionality and display the value

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderAction(_ sender: UISlider) {
+        let roundedValue: Float = round(sender.value * 100) / 100.0
+        let text: String = "\(roundedValue)m"
+        heightLabel.text = text
+    }
+    
+    @IBAction func weightSliderAction(_ sender: UISlider) {
+        let roundedValue: Int = Int(sender.value)
+        let text: String = "\(roundedValue)Kg"
+        weightLabel.text = text
+    }
 }
 
